d3e5d843e815b9cafa9702168b1d2a778a149b16
hellozimi <hello@zimi.se>
2012-12-18 21:21:49 +0100

    Initial commit

---
+++ HCAnimator/HCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HCAppDelegate.h
+//  HCAnimator
+//
+//  Created by Simon Andersson on 12/18/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HCAnimator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HCAnimator
+//
+//  Created by Simon Andersson on 12/18/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HCAppDelegate class]));
+    }
+}
