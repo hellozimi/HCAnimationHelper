a35364a2e4aba193901a48c10756fc0ffddf523b
hellozimi <hello@zimi.se>
2012-12-18 21:27:17 +0100

    added view controller class

---
+++ HCAnimator/Classes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HCAnimatorExample
+//
+//  Created by Simon Andersson on 12/18/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HCAnimator/Classes/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  HCAnimatorExample
+//
+//  Created by Simon Andersson on 12/18/12.
+//  Copyright (c) 2012 hiddencode.me. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
